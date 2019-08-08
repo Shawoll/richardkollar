a70da7b7bb6c16b7697d39430746dfea57298817
20x20+20 <rchrdkollar@gmail.com>
2019-08-08 21:28:33 +0200

    modifications

--- rchrdKOLLAR.Constants/Constants.cs
+++ rchrdKOLLAR.Constants/Constants.cs
@@ -6,11 +6,8 @@ namespace rchrdKOLLAR.Constants
     {
         public const string githubAPI = @"https://api.github.com/";
         public const string githubAPIUser = @"https://api.github.com/users/";
-
         public const string fbAPI = @"https://api.facebook.com/";
-
         public const string twAPI = @"https://api.twitter.com/";
-
         public const string igAPI = @"https://api.instagram.com/";
     }
 }
